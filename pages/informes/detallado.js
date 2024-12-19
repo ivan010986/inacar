7b615406ec3d4bf80317ebccb699db9296087357
Duvan0816 <duvanobed@gmail.com>
2024-12-19 12:28:56 -0500

    Update detallado.js

--- pages/informes/detallado.js
+++ pages/informes/detallado.js
@@ -130,7 +130,7 @@ const Detallado = () => {
         const data = await presupuestosResponse.json();
         allData = [...allData, ...data.results]; // Concatenate new data
 
-        totalPages = Math.ceil(data.count / 2000); 
+        totalPages = Math.ceil(data.count / 1500); 
         page++; // Move to the next page
       } while (page <= totalPages);
       
